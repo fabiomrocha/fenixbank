0603d6e18f3d38076b82888d3229ce3aae3b8887
Leandro Veloso <leandrovelosorodrigues@gmail.com>
2022-07-27 15:40:25 -0300

    US02 - Refatoração de código

--- js/validacaoData.js
+++ js/validacaoData.js
@@ -1,23 +1,21 @@
 //US 002 -  Valida data de nascimento do cliente
 function verificaSeDataNascimentoInvalida (cliente){
-    // Valida se a data está vazia ou nula (Inválida)
+    // Valida se a data está incompleta, vazia ou nula (Inválida)
     if(cliente.dtNascimento === "" || cliente.dtNascimento === null)
         return "Data Incompleta";
 
     
     // Separando a data recebida do formulário pelo caractere '-' e adicionando em uma variável
-    let dataNascimentoCompleta = cliente.dtNascimento.split('-');
+    const dataNascimentoCompleta = cliente.dtNascimento.split('-');
 
     // Separando a data e fazendo conversão para inteiro
-    let anoNascimento = parseInt(dataNascimentoCompleta[0]);
-    let mesNascimento = parseInt(dataNascimentoCompleta[1]);
-    let diaNascimento = parseInt(dataNascimentoCompleta[2]);
+    const anoNascimento = parseInt(dataNascimentoCompleta[0]);
+    const mesNascimento = parseInt(dataNascimentoCompleta[1]);
+    const diaNascimento = parseInt(dataNascimentoCompleta[2]);
 
-    let anoAtual = new Date().getFullYear();
-    let mesAtual = new Date().getMonth()+1; // +1 pois o mês começa em 0
-    let diaAtual = new Date().getDate();
-
-    let aniversarioEsteAno = false;
+    const anoAtual = new Date().getFullYear();
+    const mesAtual = new Date().getMonth()+1; // +1 pois o mês começa em 0
+    const diaAtual = new Date().getDate();
 
     let idade = 0;
 
@@ -25,27 +23,23 @@ function verificaSeDataNascimentoInvalida (cliente){
     if((anoNascimento > anoAtual)
     || (anoNascimento === anoAtual && mesNascimento > mesAtual)
     || (anoNascimento === anoAtual && (mesNascimento >= mesAtual && diaNascimento > diaAtual)))
-        return "Data superior não aceita!";
+        return "Data futura!";
 
-    // Verifica se o mês de nascimento é menor ao mês atual. 
-    // Em caso positivo significa que o usuário já fez aniversário este ano
-    if(mesNascimento < mesAtual)
-        aniversarioEsteAno = true;
-    
-    // Verifica se o mês de nascimento é igual ao mês atual. 
-    // Em caso positivo é necessário validar o dia para então dizer se o usuário já fez aniversário este ano
-    if(mesNascimento === mesAtual)
-        if(diaNascimento <= diaAtual)
-            aniversarioEsteAno = true;
-    
-    if(aniversarioEsteAno)
-        idade = anoAtual - anoNascimento ;
+    // Verifica se o mês de nascimento é menor que o mês atual. 
+    // OU (||) Verifica se o mês de nascimento é o atual E o dia de nascimento é menor ou igual a hoje
+    // Caso algum dis critérios seja verdadeiro, então o usuário já fez aniversário este ano
+    if((mesNascimento < mesAtual) 
+    || (mesNascimento === mesAtual && (diaNascimento <= diaAtual)))
+        //Se o usuário já fez aniversário este ano a sua idade será a subtração do ano atual com seu ano de nascimento
+        idade = anoAtual - anoNascimento;
     else
-        idade = anoAtual - anoNascimento - 1; //Se o usuário não fez aniversário este ano devemos subtrair 1 ano da idade
-
+        //Se o usuário não fez aniversário este ano devemos subtrair 1 ano da idade
+        idade = anoAtual - anoNascimento - 1; 
+    
+    
     // Retorna se a data informada representa alguém com menos de 18 anos (true) ou não (false)
     if(idade < 18)
-        return "Cliente menor de idade!";
+        return "Menor de idade!";
     else
         return "Ok";
 }
